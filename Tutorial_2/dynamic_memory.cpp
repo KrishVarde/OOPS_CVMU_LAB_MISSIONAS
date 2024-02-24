7663dcceef01b873c4b8aab1e6dd42387f5c4a5c
KrishVarde <143574052+KrishVarde@users.noreply.github.com>
2024-02-24 22:00:36 +0530

    tutorial 2

--- Tutorial_2/dynamic_memory.cpp
+++ Tutorial_2/dynamic_memory.cpp
@@ -1,15 +1,43 @@
-#include<iostream>
+#include <iostream>
 
-class memory
-{
+class Memory {
 private:
-    
+    int *arr;
+    int size;
 public:
-    void deallocate(void);
-    void create(void);
+    Memory() : arr(nullptr), size(0) {} // Constructor to initialize arr to nullptr
+
+    ~Memory() { // Destructor to deallocate memory
+        delete[] arr;
+    }
+
+    void create(){
+        std::cout << "Please enter array size: ";
+        std::cin >> size;
+        
+        // Allocate memory for the array
+        arr = new int[size];
+
+        std::cout << "Please enter the elements:" << std::endl;
+        for (int i = 0; i < size; ++i)
+        {
+            std::cin >> arr[i];
+        }
+
+        std::cout << "Array size: " << size << std::endl;
+
+        // Display the elements
+        std::cout << "Array elements: ";
+        for (int i = 0; i < size; ++i)
+        {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 };
 
 int main(){
-
+    Memory m1;
+    m1.create();
     return 0;
 }
