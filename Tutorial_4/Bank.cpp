b5ea777acb5247c1cbc3139374eb7e842e05ad6c
KrishVarde <143574052+KrishVarde@users.noreply.github.com>
2024-02-27 18:50:43 +0530

    BAnk

--- Tutorial_4/Bank.cpp
+++ Tutorial_4/Bank.cpp
@@ -1,36 +1,69 @@
 #include<iostream>
 #include<string>
-class Bank
-{
-private:
-    char name[15];
-    char acc_type[10];
-    int acc_num;
-    int balance;
-    //extra not in the question
-    std::string password[8];
-public:
-    void bal(void);
-    bool login(std::string& in);
-
-
+class Bank{
+    private:
+    std::string name;
+    std::string type;
+    int bal=0;
+    public:
+    int info(void);
+    int add_acc(void);
+    int deposit(void);
+    int widraw(void);
 };
 
-bool Bank::login(std:: string & in){
-if (in == password) {
-        std::cout << "Login successful!" << std::endl;
-        return true;
-    } else {
-        std::cout << "Login failed. Incorrect password." << std::endl;
-        return false;
+int Bank::deposit(){
+    int fleg=0;
+    std::cout<<std::endl<<"please enter the diposital amount: ";
+    std::cin>>fleg;
+    bal+=fleg;
+    std::cout<<std::endl<<"Your current Bank belance is ";
+    std::cout<<bal;
+    return 1;
+}
+
+
+int Bank::add_acc(void){
+    std::cout << std::endl<<"Please enter your name: ";
+    std::getline(std::cin, name);  // Use std::getline to read the full name
+    std::cout <<std::endl<< "Please add account type: ";
+    std::getline(std::cin,type);
+    std::cout <<std::endl<< "Account added successfully!" << std::endl;
+
+}
+
+int Bank::widraw(void){
+    int fleg=0;
+    std::cout<<std::endl<<"Please enter the Widrawal amount: ";
+    std::cin>>fleg;
+    if (fleg>bal)
+    {
+        std::cout<<"You don't have such balence!";
+        exit(404);
     }
+    
+    bal-=fleg;
+    std::cout<<std::endl<<"Your current Bank belance is ";
+    std::cout<<bal;
+    return 1;
 }
 
-void Bank::bal(void){
-    int check=0;
 
-    std::cout<<""<<name;
+int Bank::info(void){
+    std::cout<<"customer name: ";
+    std::cout<<name;
+    std::cout<<std::endl<<"Bank Balence: ";
+    std::cout<<bal;
+    std::cout<<std::endl<<"Bank Account type: ";
+    std::cout<<type;
+    return 1;
 }
+
 int main(){
+    Bank c1;
+    c1.add_acc();
+    c1.info();
+    c1.deposit();
+    c1.widraw();
     return 0;
 }
